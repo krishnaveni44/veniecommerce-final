388de25e4d7d7b58d57c599e2d583bca8225f629
krishnaveni44 <krishnaveni44@gmail.com>
2022-07-16 14:20:49 +0530

    ECommerce

---
+++ db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+
+var mongoDBURL = 'mongodb+srv://moviesdatabase:welcome123@cluster0.aiiaa.mongodb.net/mernecommerce'
+
+mongoose.connect(mongoDBURL , {useUnifiedTopology:true , useNewUrlParser:true})
+
+var dbconnect = mongoose.connection
+
+dbconnect.on('error' , ()=>{
+    console.log(`Mongo DB Connection Failed`);
+})
+
+dbconnect.on('connected' , ()=>{
+    console.log(`Mongo DB Connection Successfull`);
+})
+
+module.exports = mongoose
