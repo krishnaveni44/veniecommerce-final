8d4348fb6bebb26331a7a4a688a867c4654d7d83
krishnaveni44 <krishnaveni44@gmail.com>
2023-01-09 12:00:52 +0530

    1

--- server/db.js
+++ server/db.js
@@ -4,16 +4,18 @@ const mongoose = require("mongoose");
 var mongoDBURL = 'mongodb+srv://moviesdatabase:welcome123@cluster0.aiiaa.mongodb.net/mernecommerce'
 
 
-mongoose.connect(mongoDBURL , {useUnifiedTopology:true , useNewUrlParser:true})
+mongoose.connect(mongoDBURL , {useUnifiedTopology:true , useNewUrlParser:true}).then(()=>console.log('Connected to db'))
+.catch((err)=> console.log("DB connection error",err));
 
-var dbconnect = mongoose.connection
 
-dbconnect.on('error' , ()=>{
-    console.log(`Mongo DB Connection Failed`);
-})
+// var dbconnect = mongoose.connection
 
-dbconnect.on('connected' , ()=>{
-    console.log(`Mongo DB Connection Successfull`);
-})
+// dbconnect.on('error' , ()=>{
+//     console.log(`Mongo DB Connection Failed`);
+// })
+
+// dbconnect.on('connected' , ()=>{
+//     console.log(`Mongo DB Connection Successfull`);
+// })
 
 module.exports = mongoose
