bbb8967f17934f1f6ce6539d51afe34793dfcf8f
krishnaveni44 <krishnaveni44@gmail.com>
2023-01-09 12:21:37 +0530

    2

--- server/server.js
+++ server/server.js
@@ -12,18 +12,18 @@ app.use('/api/users/',userRoute)
 app.use('/api/orders/',orderRoute)
 
 
-    if(process.env.NODE_ENV === 'production')
-    {
-        app.use('/' , express.static('client/build'))
+    // if(process.env.NODE_ENV === 'production')
+    // {
+    //     app.use('/' , express.static('client/build'))
 
-        app.get('*' , (req,res)=>{
+    //     app.get('*' , (req,res)=>{
 
-            res.sendFile(path.resolve(__dirname , 'client/build/index.html'))
+    //         res.sendFile(path.resolve(__dirname , 'client/build/index.html'))
 
-        })
+    //     })
 
 
-    }
+    // }
 
    
 
